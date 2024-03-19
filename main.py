53b41986650977557c161705eeb88cb3a2174e5e
Jakub <jakubplewa00@gmail.com>
2024-03-19 19:54:03 +0100

    0.2

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import tkinter as tk
-from tkinter import *
+from tkinter import filedialog, IntVar, StringVar
+from os.path import expanduser
+
 
 # Okno glowne
 window = tk.Tk()
@@ -8,9 +10,28 @@ window.geometry("720x720")
 window.resizable(False, False)
 window.configure(bg="#606166")
 
+
+#funkcje
+
+def browse_input_file():
+    input_file_path = filedialog.asksaveasfilename(defaultextension=".txt", filetypes=[("TXT files", "*.txt")], initialdir=expanduser("~") + "/Desktop/")
+    if input_file_path:
+        entry_file_path.delete(0, tk.END)
+        entry_file_path.insert(0, input_file_path)
+#Funkcja generate
+def generate_chart():
+    input_data = entry_file_path.get()
+    with open(input_data, "r") as myfile:
+        for el in myfile:
+            print(el)
+
+
+
+
+
 # Wybieranie metody liczenia
 button_frame = tk.Frame(window, width=720, height=720)
-button_frame.configure(bg="#606166", highlightbackground="black", highlightthickness="1")
+button_frame.configure(bg="#606166")
 button_frame.pack(pady = 50, padx=30)
 
 input_label_methods = tk.Label(button_frame, text="Pick the calculation method")
@@ -30,7 +51,23 @@ method3_button.grid(row=1, column=2, padx=20, pady=5)
 # input_label_data = tk.Label(window, text="Choose ")
 input_button = tk.Button(button_frame, text="Input your data")
 input_button.grid(row=2, padx=20, pady=5, columnspan=3)
-file_button = tk.Button(button_frame, text="Choose from file")
+file_button = tk.Button(button_frame, text="Choose from file", command = browse_input_file)
 file_button.grid(row=3, column=1, padx=20, pady=5)
+
+text = StringVar(button_frame)
+file_path = StringVar(button_frame)
+
+# Wpisywanie sciezki pliku
+entry_file_path = tk.Entry(button_frame, textvariable=file_path)
+entry_file_path.grid(row=3, column=2)
+
+#zakres
+input_range = tk.Entry(button_frame, textvariable=text)
+input_range.grid(row = 4, columnspan=3)
+
+#generator
+generate_button = tk.Button(button_frame, text="Generate Chart", command=generate_chart)
+generate_button.grid(row=5, columnspan=1, pady=20)
+
 
 window.mainloop()
